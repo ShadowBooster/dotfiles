0ef1dde17f654ed7228e079e4ef6ce9703c6a9f5
Evelyn <victor.v.d.sar@gmail.com>
2025-05-28 02:52:38 +0200

    clean home.nix

--- home.nix
+++ home.nix
@@ -6,17 +6,8 @@
   home.username = "evelynvds";
   home.homeDirectory = "/home/evelynvds";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
