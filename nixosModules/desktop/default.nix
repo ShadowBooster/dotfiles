09a58ff263c51c2c49a2738d990914404ec09656
Evelyn <victor.v.d.sar@gmail.com>
2025-09-30 20:18:22 +0200

    simplify

--- nixosModules/desktop/default.nix
+++ nixosModules/desktop/default.nix
@@ -8,21 +8,19 @@
 
 {
   options = {
-    desktop.enable = lib.mkEnableOption "enables desktop environment";
+    desktop.enable = lib.mkEnableOption "plasma6";
 
-    desktop.environment = lib.mkOption {
-      type = lib.types.str;
-      default = "plasma6";
-      description = "Desktop environment to use (e.g. plasma6, gnome, xfce)";
-    };
+    # currently only plasma6
+    # desktop.environment = lib.mkOption {
+    #   type = lib.types.str;
+    #   default = "plasma6";
+    #   description = "Desktop environment to use (e.g. plasma6, gnome, xfce)";
+    # };
   };
 
   config = lib.mkIf config.desktop.enable {
-    imports = [
-      (lib.mkIf (config.desktop.environment == "plasma6") ./plasma6.nix)
-      # If i want to switch
-      # (lib.mkIf (config.desktop.environment == "gnome") ./gnome.nix)
-      # (lib.mkIf (config.desktop.environment == "xfce") ./xfce.nix)
-    ];
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+    qt.platformTheme = "kde";
   };
 }
